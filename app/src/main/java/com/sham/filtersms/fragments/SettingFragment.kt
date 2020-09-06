678f9028d1ec3d0ce8c7f01dc6ede558f5706db9
Shamera Ranathunga <shamera.ranathunga@auckland.ac.nz>
2020-09-06 20:07:08 +1200

    add select view

--- app/src/main/java/com/sham/filtersms/fragments/SettingFragment.kt
+++ app/src/main/java/com/sham/filtersms/fragments/SettingFragment.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
@@ -14,11 +17,14 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.sham.filtersms.FilterSMS
 import com.sham.filtersms.R
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 import kotlinx.android.synthetic.main.fragment_settings.view.*
 
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(), AdapterView.OnItemClickListener {
+
+    val siran_tune_select = "" // need to get item from "override fun onItemClick()"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +39,16 @@ class SettingFragment : Fragment() {
         // Inflate the layout for this fragment
         var v = inflater.inflate(R.layout.fragment_settings, container, false)
 
-//        val ref = FirebaseDatabase.getInstance().getReference("sms_setting")
+        var listView:ListView? = null
+        var arrayAdapter:ArrayAdapter<String>? = null
+
+        listView = v.ListViewSiran
+        arrayAdapter = ArrayAdapter(v.context, android.R.layout.simple_list_item_multiple_choice,resources.getStringArray(R.array.siran_tunes_list))
+        listView?.adapter = arrayAdapter
+        listView?.choiceMode = ListView.CHOICE_MODE_SINGLE
+        listView?.onItemClickListener = this
+
+        //init db connection
         val ref = FirebaseDatabase.getInstance().reference
 
         var getdatafilersms = object : ValueEventListener{
@@ -57,7 +72,6 @@ class SettingFragment : Fragment() {
         }
         ref.addValueEventListener(getdatafilersms)
         ref.addListenerForSingleValueEvent(getdatafilersms)
-
 
         v.btnApply.setOnClickListener ({
             // Write a message to the database
@@ -88,5 +102,10 @@ class SettingFragment : Fragment() {
 
         })
         return v
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var item:String = p0?.getItemAtPosition(p2) as String
+        Toast.makeText(context, "Select $item", Toast.LENGTH_SHORT).show()
     }
 }
